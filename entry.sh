569adc46309eacd31e8545ef8cdcc01f911506e4
Chris Fordham <chris@fordham.id.au>
2023-06-24 10:37:57 +1000

    Improve dbus implementation.

--- entry.sh
+++ entry.sh
@@ -19,11 +19,14 @@ ssh-keygen -A
 echo 'starting sshd...'
 /usr/sbin/sshd -D &
 
-echo 'starting dbus-daemon..'
+echo 'starting dbus-daemon...'
 systemd-machine-id-setup
-/usr/bin/dbus-daemon --system --fork --nopidfile --address="unix:path=/run/dbus/socket"
-echo "export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/socket" > /etc/profile.d/dbus.sh
-chmod +x -v /etc/profile.d/dbus.sh
+/usr/bin/dbus-daemon --system --fork --nopidfile --address="unix:path=/run/dbus/system_bus_socket"
+
+echo 'adding system profiles...'
+echo "export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket" > /etc/profile.d/dbus.sh
+echo "export FONTCONFIG_PATH=/etc/fonts" > /etc/profile.d/fonts.sh
+chmod +x -v /etc/profile.d/dbus.sh /etc/profile.d/fonts.sh
 
 echo 'starting x2gosessions...'
 /usr/sbin/x2gocleansessions --debug
